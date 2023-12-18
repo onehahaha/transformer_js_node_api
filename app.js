eff9b15c8d16a3009ff06869fbee7cdfa60eedd7
xiaqixiang <xiaqixiang00@163.com>
2023-12-18 13:10:01 +0800

    feat: 删除多余接口

--- app.js
+++ app.js
@@ -14,24 +14,6 @@ const port = 3411;
 // 使用用户路由
 app.use("/apis", apiRoutes);
 
-app.post("/apis/speechToText", async (req, res) => {
-  console.log("req", req.body);
-  const currentDir = getCurrentPath(import.meta.url);
-  // Load audio data
-  const file = path.join(currentDir, "public", "jfk.wav");
-
-  try {
-    const result = await getVoiceToTextResult(file);
-    res.send({
-      data: result,
-    });
-  } catch (error) {
-    res.send({
-      data: error.message,
-    });
-  }
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
